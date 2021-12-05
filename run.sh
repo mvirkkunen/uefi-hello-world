2ce75ff8081eae2c5d55f40e7114d19011eaa89a
Matti Virkkunen <mvirkkunen@gmail.com>
2021-12-06 00:38:18 +0200

    Remove qemu warning

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ rm -f image/efi/boot/boot64.efi
 fasm efi64.asm image/efi/boot/boot64.efi
 echo "boot64.efi" > image/efi/boot/startup.nsh
 
-qemu-system-x86_64 -bios OVMF.fd -hda fat:rw:image
+qemu-system-x86_64 -bios OVMF.fd -drive file=fat:rw:image,media=disk,format=raw
